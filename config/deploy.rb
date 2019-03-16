96fbd7cb05f0ec6a0a35a7649509615fba7aac9e
chiezoo1991 <chienakayama2012eb@gmail.com>
2019-03-16 19:56:05 +0900

    2.5.1
    Rubyのver変更した

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock "~> 3.11.0"
+lock "3.11.0"
 
 set :application, "chat-space2"
 set :repo_url, "git@github.com:chiezoo1991/chat-space2.git"
@@ -7,7 +7,7 @@ set :repo_url, "git@github.com:chiezoo1991/chat-space2.git"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.3.1'
+set :rbenv_ruby, '2.5.1'
 
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['~/.ssh/gyukaku8_pem.pem']
