f0b4ed011f80590707ba07f9d44529a8a24d46c5
chiezoo1991 <chienakayama2012eb@gmail.com>
2019-03-18 21:08:25 +0900

    $ajax.add
    Ajax関数を加えた

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,16 @@
+$(function(){
+  $('#form__message').on('submit', function(e){
+    var formData = new FormData(this);
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+  })
+})
+
