0ef63d5ef6db43b19bec669b1317b16474ba37a2
chiezoo1991 <chienakayama2012eb@gmail.com>
2019-04-09 21:41:26 +0900

    Filename.change

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,10 +1,11 @@
 $(document).on('turbolinks:load', function() {
   //テンプレートリテラル記法
    function buildHTML(message){
-    var addImage = '';
-    if (message.image.url) {
-      addImage = `<img src="${message.image.url}" class="lower-message__image">`;
-    }
+    var addImage = (message.image !== null) ? `<img class = "image_size", src="${message.image}">` : ''
+    // var addImage = '';
+    // if (message.image.url) {
+    //   addImage = `<img src="${message.image.url}" class="lower-message__image">`;
+    // }
     var html = `
         <div class="message" data-message-id="${message.id}">
           <div class="upper-message" data-message-id="${message.id}">
@@ -37,11 +38,12 @@ $(document).on('turbolinks:load', function() {
       $('.messages').append(html)
       $('.form__message').val('')
       $('.formsubmit').removeAttr('data-disable-with');
-      $('.messeage').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
-      console.log(html);
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+      $('form')[0].reset();
     })
     .fail(function(message){
       alert('メッセージを入力してください');
   })
+    return false;
  })
 })
