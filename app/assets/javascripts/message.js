19e6ed858cc6881195a37107c86abc584e4d14c4
chiezoo1991 <chienakayama2012eb@gmail.com>
2019-05-05 19:27:59 +0900

    Image.appear
    画像をリロードしなくても出す。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,12 +1,7 @@
 $(document).on('turbolinks:load', function() {
-  //テンプレートリテラル記法
    function buildHTML(message){
-    var addImage = (message.image !== null) ? `<img class = "image_size", src="${message.image}">` : ''
-    // var addImage = '';
-    // if (message.image.url) {
-    //   addImage = `<img src="${message.image.url}" class="lower-message__image">`;
-    // }
-    var html = `
+    var addImage = (message.image.url !== null) ? `<img class = "image_size", src="${message.image.url}">` : ''
+     var html = `
         <div class="message" data-message-id="${message.id}">
           <div class="upper-message" data-message-id="${message.id}">
             <div class="upper-message__user-name">${message.name}</div>
@@ -47,33 +42,3 @@ $(document).on('turbolinks:load', function() {
     return false;
 })
 });
-
-//   $(function(){
-//     function buildMESSAGE(message) {
-//       var messages = $('tbody').append('<tr class="messages" data-id=' + message.id + '><td>' + message.text + '</td><td><a href="/messages/' + message.id + '">Show</a></td><td><a href="/messages/' + message.id +'/edit">Edit</a></td><td><a data-confirm="Are you sure?" rel="nofollow" data-method="delete" href="/messages/' + message.id + '">Destroy</a></td>');
-//     }
-//   $(function(){
-//     setInterval(update, 10000);
-//   });
-//   function update(){
-//     if($('.messages')[0]{
-//       var message_id = $('.messages:last').data('id');
-//     } else {
-//       var message_id = 0
-//     })
-//     $.ajax({
-//       url: location.href,
-//       type: 'GET',
-//       data: {
-//         message: { id: message_id }
-//       },
-//       dataType: 'json'
-//     })
-//     .always(function(data){
-//       $.each(data, function(i, data){
-//         buildMESSAGE(data);
-//       });
-//     });
-//   }
-// });
-// }
