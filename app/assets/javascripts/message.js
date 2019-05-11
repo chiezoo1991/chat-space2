a3c5afb02e549dfc2fbeb55ec7e87d1d44b9d292
chiezoo1991 <chienakayama2012eb@gmail.com>
2019-05-11 11:17:41 +0900

    Edit.bag

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,15 +1,16 @@
 $(document).on('turbolinks:load', function() {
    function buildHTML(message){
+    var content = message.content? `${message.content}` : "";
     var addImage = (message.image.url !== null) ? `<img class = "image_size", src="${message.image.url}">` : ''
      var html = `
-        <div class="message" data-messageId="${message.id}" data-groupId="${message.group_id}">
-         <div class="upper-message" data-messageId="${message.id}">
+        <div class="message" data-id="${message.id}">
+         <div class="upper-message">
             <div class="upper-message__user-name">${message.name}</div>
             <div class="upper-message__date">${message.date}</div>
           </div>
           <div class="lower-meesage">
             <p class="lower-message__content">
-              ${message.content}
+              ${content}
             </p>
             ${addImage}
           </div>
@@ -20,8 +21,9 @@ $(document).on('turbolinks:load', function() {
   $('.new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
+    var url = $(this).attr('action')
     $.ajax({
-      url: location.href,
+      url: url,
       type: "POST",
       data: formData,
       dataType: 'json',
@@ -41,10 +43,17 @@ $(document).on('turbolinks:load', function() {
     return false;
 })
 var reloadMessages = function() {
-    var last_message_id = $('.message').last().attr("data-messageId");
-    var groupId = $('.message').last().attr("data-groupId");
+   last_message_id = $('.message:last').data('id');
+   if (last_message_id === undefined) {
+   last_message_id =0 ;
+   }
+   group_id = $('.messages').data('group');
+   url = '/groups/' + group_id + '/api/messages';
+   if (group_id === undefined) {
+   last_message_id =0
+   }
     $.ajax( {
-      url: `/groups/`+ groupId +`/api/messages`,
+      url: url,
       type: 'GET',
       data: {id: last_message_id},
       dataType: 'json',
@@ -53,7 +62,7 @@ var reloadMessages = function() {
       $.each(data, function(i, message) {
         var insertHTML = buildHTML(message);
         $('.messages').append(insertHTML);
-        $(".messages").animate({scrollTop: $(".messages")[0].scrollHeight+100}, "fast");
+        $(".messages").animate({scrollTop: $(".messages")[0].scrollHeight}, "fast");
       })
       })
   }
